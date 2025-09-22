485c27adc18255eb3002fec7fd3f0d99382ca50c
Daniyar631922 <dani.ra.95@mail.ru>
2025-09-22 13:59:29 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту треугольника N:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Треугольник высотой {n} строк:");
+
+            for (int i=1; i<=n; i++)
+            {
+                Console.Write(new string(' ', n - i));
+                Console.WriteLine(new string('*', 2 * i - 1));
+            }
+            Console.ReadLine();
+        }
+    }
+}
